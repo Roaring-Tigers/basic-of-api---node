6e7c0f1150b2e83aa4793b9f8814af21d39028be
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-12-09 21:56:29 +0530

    Refactor app.js: Added middleware and endpoints for hello and sum

--- app.js
+++ app.js
@@ -3,24 +3,54 @@ import cors from "cors";
 const app = express()
 const PORT = 5000
 
-
-
 app.use(express.json()) // middleware
-
 app.use(cors()) // middleware
 
-app.get("/hello" , (req, res)=>{
-     res.json({message: "Hello World"})
-})
-
-app.post("/sum" , (req, res)=>{
-    console.log("input",req.body)
-    const {num1,num2} = req.body
-    const sum = +num1 + +num2
-    res.json({message: "Sum is " + sum})
 
 
-})
+
+function hello(req, res){
+     res.json({"message": "Hello World"})
+}
+
+app.get("/hello", hello)
+
+// xyz.com/hello
+
+
+function sumOf2(req, res){
+    
+    let num1 = req.body.num1
+    let num2 = req.body.num2 
+
+    let sum = +num1 + +num2
+    res.json({"sum": sum})
+
+}
+
+
+app.post("/sum", sumOf2)
+
+// xyz.com/sum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(PORT, ()=>{
